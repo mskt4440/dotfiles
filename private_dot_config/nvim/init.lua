4d52273cc046badc2726c9cdc0f0b42ac07fc1f0
Masa Kato <mskt4440@gmail.com>
2023-10-25 18:36:10 +0900

    add nvim/init.lua

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,15 @@
+-- Edit
+vim.opt.number = true
+vim.opt.autoindent = true
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrapscan = true
+vim.opt.hlsearch = true
+-- Tab
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ','
